676d41d4fc7cb685a339d5ad3de3a9c8e6b3d57a
matthp9 <matt32375@gmail.com>
2024-07-13 18:19:27 -0700

    feat: Integrate UI with react-router-dom

--- org/apps/call-already/src/app/app.tsx
+++ org/apps/call-already/src/app/app.tsx
@@ -1,16 +1,24 @@
 import styled from 'styled-components';
-
-import NxWelcome from './nx-welcome';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { WelcomePage } from '../pages';
+import { StrictMode } from 'react';
 
 const StyledApp = styled.div`
   // Your style here
 `;
 
+const browserRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <WelcomePage />,
+  },
+]);
+
 export function App() {
   return (
-    <StyledApp>
-      <NxWelcome title="call-already" />
-    </StyledApp>
+    <StrictMode>
+      <RouterProvider router={browserRouter} />
+    </StrictMode>
   );
 }
 
