efddc0881cd36ef4f33e020a9afa338098a813ce
matthp9 <matt32375@gmail.com>
2024-07-17 16:01:59 -0700

    Take react-timezone-select as a dependency and test

--- org/apps/call-already/src/state/userState.ts
+++ org/apps/call-already/src/state/userState.ts
@@ -5,8 +5,16 @@ const { persistAtom } = recoilPersist();
 
 export const NICKNAME_STATE_KEY = "nickname";
 
+export const TIMEZONE_STATE_KEY = "myTimezone";
+
 export const nicknameState = atom({
   key: NICKNAME_STATE_KEY,
+  default: "",
+  effects: [persistAtom],
+});
+
+export const timezoneState = atom({
+  key: TIMEZONE_STATE_KEY,
   default: "",
   effects: [persistAtom],
 });
