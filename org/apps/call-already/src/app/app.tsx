4805a872575e386043ef701bf50f5b1a07736fdd
matthp9 <matt32375@gmail.com>
2024-07-18 09:56:17 -0700

    Install and test Mixpanel client

--- org/apps/call-already/src/app/app.tsx
+++ org/apps/call-already/src/app/app.tsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import { StrictMode } from "react";
 import { CallAlreadyRouter } from "./CallAlreadyRouter";
 import { RecoilRoot } from "recoil";
+import mixpanel from 'mixpanel-browser';
+
+mixpanel.init('c429828f09fb8a51b70524f0dba6e05f', {debug: true, track_pageview: true, persistence: 'localStorage'});
 
 const StyledApp = styled.div`
   // Your style here
--- org/apps/call-already/src/pages/WelcomePage.tsx
+++ org/apps/call-already/src/pages/WelcomePage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useSetRecoilState } from "recoil";
 import { nicknameState } from "../state";
+import mixpanel from 'mixpanel-browser';
 
 export function WelcomePage() {
   const setNickNameState = useSetRecoilState(nicknameState);
 
   const onChange = () => {
     setNickNameState("Matty");
+    mixpanel.track("Flow started");
   };
 
   return (
