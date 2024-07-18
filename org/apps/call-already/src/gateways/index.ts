99882cbac17ff09f84e6a3a0ed951b7531cdd373
matthp9 <matt32375@gmail.com>
2024-07-18 13:35:49 -0700

    Create apiGateway for accessing the backend

---
+++ org/apps/call-already/src/gateways/index.ts
@@ -0,0 +1,1 @@
+export { postResponses } from "./apiGateway";
--- org/apps/call-already/src/pages/ReviewPage.tsx
+++ org/apps/call-already/src/pages/ReviewPage.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { postResponses } from "../gateways";
 
 export function ReviewPage() {
+
+  async function onChange() {
+    const serverResponse = await postResponses();
+    console.log(serverResponse);
+  };
+
   return (
     <>
       <h1>This is the Review page</h1>
+      <button onClick={onChange}>Confirm call</button>
     </>
   );
 }
