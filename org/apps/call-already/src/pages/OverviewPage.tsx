6caff99daaae23d6f60a8f499fe5b206e29c587f
matthp9 <matt32375@gmail.com>
2024-07-20 00:15:51 -0700

    Create content for Overview page

--- org/apps/call-already/src/pages/OverviewPage.tsx
+++ org/apps/call-already/src/pages/OverviewPage.tsx
@@ -1,14 +1,33 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { nicknameState } from "../state";
+import { GROUP_ROUTE } from "../utils";
 
 export function OverviewPage() {
   const nickname = useRecoilValue(nicknameState);
   console.log(nickname);
+  const navigate = useNavigate();
+
+  const header = "How it Works";
+  const step1 = "Create a new call group or join an existing one using a room code";
+  const step2 = "Share the room with your friends and choose the times you're available to call them";
+  const step3 = "After everyone has finished, everyone receives a email with the best time to call each other!"
+  const submitText = "Get Started";
+
+  const onSubmit = () => {
+    navigate(GROUP_ROUTE);
+  }
 
   return (
     <>
-      <h1>This is the Overview page, {nickname}</h1>
+      <h1>{header}</h1>
+      <ol>
+        <li>{step1}</li>
+        <li>{step2}</li>
+        <li>{step3}</li>
+      </ol>
+      <button onClick={onSubmit}>{submitText}</button>
     </>
   );
 }
--- org/apps/call-already/src/utils/utils.ts
+++ org/apps/call-already/src/utils/utils.ts
@@ -1,1 +1,3 @@
+import { useNavigate } from "react-router-dom";
+
 export const env = process.env.NODE_ENV;
