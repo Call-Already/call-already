8253a319d9205ca6774414716416510966cf7428
matthp9 <matt32375@gmail.com>
2024-07-28 14:55:27 -0700

    Test navigation on Netlify site

--- org/apps/call-already/src/pages/WelcomePage.tsx
+++ org/apps/call-already/src/pages/WelcomePage.tsx
@@ -1,20 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { nicknameState } from "../state";
-import { emitAnalytic } from "../utils";
+import { emitAnalytic, OVERVIEW_ROUTE } from "../utils";
 
 export function WelcomePage() {
   const setNickNameState = useSetRecoilState(nicknameState);
+  const navigate = useNavigate();
 
-  const onChange = () => {
+  const onClick = () => {
     setNickNameState("Matty");
     emitAnalytic("Flow started");
+    navigate(OVERVIEW_ROUTE);
   };
 
   return (
     <>
       <h1>This is the Welcome page</h1>
-      <button onClick={onChange}>Set nickname</button>
+      <button onClick={onClick}>Set nickname</button>
     </>
   );
 }
