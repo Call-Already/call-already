65b019c9fd55bb858c36a9b415892619f500f78d
matthp9 <matt32375@gmail.com>
2024-08-12 14:09:46 -0700

    Started styling the Confirmation page

--- org/apps/call-already/src/pages/ConfirmationPage.tsx
+++ org/apps/call-already/src/pages/ConfirmationPage.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Header, InfoText, Mascot, PageContainer } from "../styles";
+import { emitAnalytic, GROUP_ROUTE, useIsMobile } from "../utils";
 
 export function ConfirmationPage() {
+  const isMobile = useIsMobile();
+  const navigate = useNavigate();
+
+  const introText = "Thank you! Your responses have will be matched with your friends and we’ll let you the best time to call each other to your email.";
+  const submitText = "Make another call";
+
+  const onReturn = () => {
+    emitAnalytic("Confirmed times");
+    navigate(GROUP_ROUTE);
+  };
+
   return (
-    <>
-      <h1>This is the Confirmation page</h1>
-    </>
+    <PageContainer $isMobile={isMobile}>
+      <Header>{"Your times have been confirmed!"}</Header>
+      <Mascot src={"/mascot_call.png"} alt="logo" />
+      <InfoText>{introText}</InfoText>
+      <Button onClick={onReturn}>{submitText}</Button>
+    </PageContainer>
   );
 }
